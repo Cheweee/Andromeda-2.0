37377b460c336c1b97cfa687ef7d9441194eba26
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-11-22 23:07:36 +0300

    #1 - Created project with shared models;

---
+++ Andromeda.Shared/EmailConnectionSettings.cs
@@ -0,0 +1,17 @@
+namespace Andromeda.Shared
+{
+    public class EmailConnectionSettings
+    {
+        public string SMTPHost { get; set; }
+
+        public string TeamName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool NeedSSL { get; set; }
+
+        public int SSLPort { get; set; }
+    }
+}

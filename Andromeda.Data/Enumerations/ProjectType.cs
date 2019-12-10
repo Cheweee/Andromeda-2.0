ede5f152a933b9ae51e69fb0364bd33e76dddaae
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-12-10 23:19:41 +0300

    #39: Fixed lost project types enumeration;

---
+++ Andromeda.Data/Enumerations/ProjectType.cs
@@ -0,0 +1,24 @@
+namespace Andromeda.Data.Enumerations
+{
+    public enum ProjectType
+    {
+        Lection,
+        PracticalLesson,
+        LaboratoryLesson,
+        ThematicalDiscussion,
+        Consultation,
+        Exam,
+        Offest,
+        Abstract,
+        StateExam,
+        postgraduateEntranceExam,
+        Practice,
+        DepartmentManagement,
+        StudentResearchWork,
+        CourseWork,
+        GraduationQualificationManagement,
+        MasterProgramManagement,
+        PostgraduateProgramManagement,
+        Other
+    }
+}

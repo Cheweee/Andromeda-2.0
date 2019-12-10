ff0525f28c43d36895e85dabdcee393e09708352
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-12-11 00:04:27 +0300

    #48: Fixed api settings serialization errors; #48: Injected app settings to reset and settings update actions;

--- Andromeda.API/Program.cs
+++ Andromeda.API/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Andromeda.API
 {
@@ -22,21 +23,20 @@ namespace Andromeda.API
         public static int Main(string[] args)
         {
             var host = BuildWebHost(args);
-            var configurationBuilder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
-            var configuration = configurationBuilder.Build();
-            var settings = configuration.Get<DatabaseConnectionSettings>();
+            Appsettings appsettings = JsonConvert.DeserializeObject<Appsettings>(
+                File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"))
+            );
+
 
             return CommandLine.Parser.Default.ParseArguments<ApiOptions, MigrateUpOptions, MigrateDownOptions, DropOptions, CreateOptions, ResetOptions, SolutionSettingsOptions>(args)
             .MapResult(
                 (ApiOptions options) => RunApi(host.Services.GetService<ILogger<Program>>(), host),
-                (DropOptions options) => RunDrop(host.Services.GetService<ILogger<Drop>>(), settings, options),
-                (ResetOptions options) => RunReset(host.Services.GetService<ILogger<Reset>>(), options),
-                (CreateOptions options) => RunCreate(host.Services.GetService<ILogger<Create>>(), settings, options),
-                (MigrateUpOptions options) => RunMigrateUp(host.Services.GetService<ILogger<MigrateUp>>(), settings, options),
-                (MigrateDownOptions options) => RunMigrateDown(host.Services.GetService<ILogger<MigrateDown>>(), settings, options),
-                (SolutionSettingsOptions options) => RunSettingsUpdate(host.Services.GetService<ILogger<SettingsUpdate>>(), options),
+                (DropOptions options) => RunDrop(host.Services.GetService<ILogger<Drop>>(), appsettings.DatabaseConnectionSettings, options),
+                (ResetOptions options) => RunReset(host.Services.GetService<ILogger<Reset>>(), appsettings, options),
+                (CreateOptions options) => RunCreate(host.Services.GetService<ILogger<Create>>(), appsettings.DatabaseConnectionSettings, options),
+                (MigrateUpOptions options) => RunMigrateUp(host.Services.GetService<ILogger<MigrateUp>>(), appsettings.DatabaseConnectionSettings, options),
+                (MigrateDownOptions options) => RunMigrateDown(host.Services.GetService<ILogger<MigrateDown>>(), appsettings.DatabaseConnectionSettings, options),
+                (SolutionSettingsOptions options) => RunSettingsUpdate(host.Services.GetService<ILogger<SettingsUpdate>>(), appsettings, options),
                 errs => 1
             );
         }
@@ -67,10 +67,10 @@ namespace Andromeda.API
             }
         }
         static int RunDrop(ILogger logger, DatabaseConnectionSettings settings, DropOptions options) => Drop.Run(logger, settings);
-        static int RunReset(ILogger logger, ResetOptions options) => Reset.Run(logger, options);
+        static int RunReset(ILogger logger, Appsettings appsettings, ResetOptions options) => Reset.Run(logger, appsettings, options);
         static int RunCreate(ILogger logger, DatabaseConnectionSettings settings, CreateOptions options) => Create.Run(logger, settings);
         static int RunMigrateUp(ILogger logger, DatabaseConnectionSettings settings, MigrateUpOptions options) => MigrateUp.Run(logger, settings);
         static int RunMigrateDown(ILogger logger, DatabaseConnectionSettings settings, MigrateDownOptions options) => MigrateDown.Run(logger, settings);
-        static int RunSettingsUpdate(ILogger logger, SolutionSettingsOptions options) => SettingsUpdate.Run(logger, options);
+        static int RunSettingsUpdate(ILogger logger, Appsettings appsettings, SolutionSettingsOptions options) => SettingsUpdate.Run(logger, appsettings, options);
     }
 }

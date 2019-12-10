5e08449ff4c99843ae2e1a7e708151e327b1282f
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-12-11 00:01:44 +0300

    #47: Created discipline title controller;

---
+++ Andromeda.API/Controllers/DisciplineTitleController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Andromeda.Data.Models;
+using Andromeda.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Andromeda.API.Controllers
+{
+    [Route("api/disciplinetitle")]
+    [ApiController]
+    public class DisciplineTitleController : ControllerBase
+    {
+        private readonly DisciplineTitleService _disciplineService;
+
+        public DisciplineTitleController(DisciplineTitleService disciplineTitleService)
+        {
+            _disciplineService = disciplineTitleService ?? throw new ArgumentException(nameof(disciplineTitleService));
+        }
+
+        [HttpGet("notpinned")]
+        [Authorize]
+        public async Task<IActionResult> Get([FromQuery]DisciplineTitleGetOptions options)
+        {
+            var models = await _disciplineService.GetNotPinnedDisciplines(options);
+
+            return Ok(models);
+        }
+    }
+}

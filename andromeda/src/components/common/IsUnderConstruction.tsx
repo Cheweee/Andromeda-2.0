e00a565325a5b399517ce6f6e88fb3ae032ca842
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-12-11 08:05:33 +0300

    #60: Added icon to is under construction page;

--- andromeda/src/components/common/IsUnderConstruction.tsx
+++ andromeda/src/components/common/IsUnderConstruction.tsx
@@ -3,15 +3,17 @@ import { mergeStyles } from "../../utilities";
 import { layoutStyles } from "../../muiTheme";
 import { withStyles, WithStyles } from "@material-ui/styles";
 import { Grid, Typography } from "@material-ui/core";
+import { Receipt } from "@material-ui/icons";
 
 const styles = mergeStyles(layoutStyles);
 
 export const IsUnderConstruction = withStyles(styles)(function (props: WithStyles<typeof styles>) {
     const { classes } = props;
     return (
-        <Grid container direction="row" alignItems="center" justify="center" className={classes.root}>
+        <Grid container direction="row" alignItems="center" justify="center">
             <Grid item xs />
-            <Grid item>
+            <Grid container direction="column" item alignItems="center" justify="center">
+                <Receipt style={{ fontSize: '200px' }} />
                 <Typography variant="h5" component="h5">Извините... Страница в разработке.</Typography>
             </Grid>
             <Grid item xs />

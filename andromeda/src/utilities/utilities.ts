81a1af39296310414f0e8c52b1d41c2017e33983
Kirill Ivanchenko <kni2012.15@gmail.com>
2019-12-11 03:39:25 +0300

    #51: Created get shortening function;

--- andromeda/src/utilities/utilities.ts
+++ andromeda/src/utilities/utilities.ts
@@ -1,3 +1,17 @@
-export const capitalize = function (value: string) {
-    return value.charAt(0).toUpperCase() + value.slice(1)
+export const capitalize = function (source: string): string {
+    return source.charAt(0).toUpperCase() + source.slice(1)
+}
+
+export const getShortening = function (source: string): string {
+    let shortening: string = '';
+
+    const words = source.split(' ');
+    for (const word of words) {
+        if (word.length === 1)
+            shortening += word.charAt(0);
+        else
+            shortening += word.toUpperCase().charAt(0);
+    }
+
+    return shortening;
 }

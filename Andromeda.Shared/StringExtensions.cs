ec2e2a0a1f38126935afc44d3d67ecc5e7fe6b5c
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-01-06 11:27:00 +0300

    #63: Created string extensions for get shortening for sentence;

---
+++ Andromeda.Shared/StringExtensions.cs
@@ -0,0 +1,20 @@
+namespace Andromeda.Shared
+{
+    public static class StringExtensions
+    {
+        public static string GetShortening(this string source)
+        {
+            string shortening = "";
+
+            var words = source.Split(' ');
+            foreach (var word in words) {
+                if (word.Length == 1)
+                    shortening += word[0];
+                else
+                    shortening += word.ToUpper()[0];
+            }
+
+            return shortening;
+        }
+    }
+}

91dcf37d6b3614401c2dcc7e9e5b6e8b492486ed
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-01-06 11:38:11 +0300

    #63: Total load property is now computing by sum of study load; #63: Appended initialization to study load exception to fix null reference exception for total load; #63: Appended search by id and ids for department load get options; #63: Created department load import options;

--- Andromeda.Data/Models/DepartmentLoadModels.cs
+++ Andromeda.Data/Models/DepartmentLoadModels.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Andromeda.Data.Models
 {
@@ -7,14 +8,21 @@ namespace Andromeda.Data.Models
         public int Id { get; set; }
         public int DepartmentId { get; set; }
         public string StudyYear { get; set; }
-        public double TotalLoad { get; set; }
+        public double TotalLoad => StudyLoad.Select(o => o.Value).Sum();
 
-        public IEnumerable<StudyLoad> StudyLoad { get; set; }
+        public IEnumerable<StudyLoad> StudyLoad { get; set; } = new List<StudyLoad>();
     }
 
-    public class DepartmentLoadGetOptions
+    public class DepartmentLoadGetOptions : BaseGetOptions
     {
         public int? DepartmentId { get; set; }
         public IReadOnlyList<int> DepartmentIds { get; set; }
+    }
+
+    public class DepartmentLoadImportOptions
+    {
+        public int? DepartmentId { get; set; }
+        public string FileName { get; set; }
+        public bool UpdateDisciplinesTitles { get; set; }
     }
 }

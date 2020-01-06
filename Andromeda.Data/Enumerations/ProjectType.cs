1bbca2d767a3094044413c2dd5b4443c589d6edc
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-01-06 11:39:40 +0300

    #63: Added xml description for project type values; #63: Fixed value name;

--- Andromeda.Data/Enumerations/ProjectType.cs
+++ Andromeda.Data/Enumerations/ProjectType.cs
@@ -1,24 +1,45 @@
 namespace Andromeda.Data.Enumerations
 {
+    ///<summary>Перечисление типов работ по учебной дисциплине.</summary>
     public enum ProjectType
     {
+        ///<summary>Лекции</summary>
         Lection,
+        ///<summary>Практические занятия</summary>
         PracticalLesson,
+        ///<summary>Лабораторные занятия</summary>
         LaboratoryLesson,
+        ///<summary>Тематические дискуссии</summary>
         ThematicalDiscussion,
+        ///<summary>Консультации</summary>
         Consultation,
+        ///<summary>Экзамены</summary>
         Exam,
+        ///<summary>Зачеты</summary>
         Offest,
+        ///<summary>Рефераты</summary>
         Abstract,
+        ///<summary>Контрольные работы ЗФО</summary>
+        EsTestPapers,
+        ///<summary>Государственные экзамены</summary>
         StateExam,
-        postgraduateEntranceExam,
+        ///<summary>Вступительные экзамены в аспирантуру</summary>
+        PostgraduateEntranceExam,
+        ///<summary>Практика</summary>
         Practice,
+        ///<summary>Руководство кафедрой</summary>
         DepartmentManagement,
+        ///<summary>НИРС</summary>
         StudentResearchWork,
+        ///<summary>КР, КП</summary>
         CourseWork,
+        ///<summary>Руководство ВКР</summary>
         GraduationQualificationManagement,
+        ///<summary>Руководство программой магистратуры</summary>
         MasterProgramManagement,
+        ///<summary>Руководство программой аспирантуры</summary>
         PostgraduateProgramManagement,
+        ///<summary>Контрольные, РГР, ДЗ и др.</summary>
         Other
     }
 }

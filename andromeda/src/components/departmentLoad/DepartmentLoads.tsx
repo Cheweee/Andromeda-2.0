f29014b41a70a9416ca2811840273aa987052d20
Kirill Ivanchenko <kni2012.15@gmail.com>
2020-01-06 13:44:31 +0300

    #63: Some ugly fixes; #63: Appended import department load from excel file;

--- andromeda/src/components/departmentLoad/DepartmentLoads.tsx
+++ andromeda/src/components/departmentLoad/DepartmentLoads.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { mergeStyles } from "../../utilities";
 import { commonStyles } from "../../muiTheme";
 import { WithStyles, withStyles } from "@material-ui/styles";
-import { DepartmentLoad, Filter } from "../../models";
-import { Grid, Card, CardContent, Typography, IconButton } from "@material-ui/core";
+import { DepartmentLoad, Filter, ApplicationError, SnackbarVariant, DepartmentLoadImportOptions } from "../../models";
+import { Grid, Card, CardContent, Typography, IconButton, Tooltip, CircularProgress, CardActions } from "@material-ui/core";
 import { DepartmentLoadDetails } from "./DepartmentLoadDetails";
-import { BarChart, Search, Add } from "@material-ui/icons";
-import { SearchInput } from "../common";
+import { SearchInput, MessageSnackbar } from "../common";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import { useSnackbarState, useFilterState } from "../../hooks";
 import { paths } from "../../sharedConstants";
+import { ArrowBack, Search, Add, InsertDriveFile, Edit, Delete } from "@material-ui/icons";
+import { departmentLoadService } from "../../services/departmentLoadService";
+import { ImportLoadDetails } from "./ImportLoadDetails";
 
 const styles = mergeStyles(commonStyles);
 
@@ -21,30 +23,91 @@ export const DepartmentLoads = withStyles(styles)(withRouter(function (props: Pr
     //#region Department loads state
     const [departmentLoads, setDepartmentLoads] = useState<DepartmentLoad[]>([]);
 
-    async function getDepartmentLoads() {
+    useEffect(() => { getDepartmentLoads(); }, [props.match.params]);
 
+    async function getDepartmentLoads() {
+        try {
+            setLoading(true);
+            const departmentId = props.match.params[paths.idParameterName];
+
+            const loads = await departmentLoadService.getDepartmentLoads({
+                departmentId: departmentId
+            });
+
+            setDepartmentLoads(loads);
+        }
+        catch (error) {
+            if (error instanceof ApplicationError)
+                setSnackbar(error.message, true, SnackbarVariant.error);
+        }
+        finally {
+            setLoading(false);
+        }
     }
 
     function handleAdd() {
         const { history } = props;
         history.push(paths.getDepartmentloadPath(props.match.params[paths.idParameterName], 'create'))
     }
+
+    function handleEdit(data: DepartmentLoad) {
+        const { history } = props;
+        history.push(paths.getDepartmentloadPath(props.match.params[paths.idParameterName], `${data.id}`));
+    }
+
+    function handleDelete(id: number) {
+    }
+    //#endregion
+
+    //#region Import load state
+    const [importDetailsOpen, setImportDetailsOpen] = useState<boolean>(false);
+
+    async function handleImportDetailsAccept(options: DepartmentLoadImportOptions) {
+        try {
+            setLoading(true);
+            setImportDetailsOpen(false);
+            const departmentId: number = props.match.params[paths.idParameterName];
+
+            options.departmentId = departmentId;
+            const load = await departmentLoadService.import(options);
+            const { history } = props;
+            history.push(paths.getDepartmentloadPath(`${departmentId}`, `${load.id}`));
+        }
+        catch (error) {
+            if (error instanceof ApplicationError)
+                setSnackbar(error.message, true, SnackbarVariant.error);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
     //#endregion
 
     const [filter, setFilter] = useFilterState(Filter.initialFilter);
     const [snackbar, setSnackbar] = useSnackbarState();
 
+    const [loading, setLoading] = useState<boolean>(false);
+
     function handleSearchChange(value: string) {
         setFilter(value);
     }
 
-    const {
-        classes
-    } = props;
+    function handleBackClick() {
+        const { history } = props;
+        history.push(paths.trainingDepartmentsPath);
+    }
+
+    const { classes } = props;
+    const departmentId = props.match.params[paths.idParameterName];
+
     return (
         <Grid container direction="column">
             <Grid container direction="row" alignItems="center">
-                <BarChart color="primary" />
+                <Tooltip title="Вернуться назад">
+                    <IconButton disabled={loading} onClick={handleBackClick}>
+                        <ArrowBack />
+                    </IconButton>
+                </Tooltip>
                 <Typography>Нагрузка кафедры</Typography>
                 <Grid item xs />
                 <Search className={classes.searchIcon} />
@@ -54,27 +117,61 @@ export const DepartmentLoads = withStyles(styles)(withRouter(function (props: Pr
                     onSearchChange={handleSearchChange}
                     onSearch={getDepartmentLoads}
                 />
-                <IconButton onClick={handleAdd}>
+                <IconButton disabled={loading} onClick={handleAdd}>
                     <Add />
                 </IconButton>
+                <Tooltip title={"Экспортировать из Excel"}>
+                    <IconButton disabled={loading} onClick={() => setImportDetailsOpen(true)}>
+                        <InsertDriveFile />
+                    </IconButton>
+                </Tooltip>
             </Grid>
-            <Grid container direction="row">
-                {departmentLoads.length ? departmentLoads.map(o => {
-                    <Grid item xs={4}>
-                        <Card>
-                            <CardContent>
-                                <DepartmentLoadDetails
-                                    departmentLoad={o}
-                                />
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                }) : (
-                        <Grid container direction="row" justify="center">
-                            <Typography color="textSecondary">Нагрузка на кафедру еще не была выделена.</Typography>
+            {loading && (
+                <Grid container direction="row" justify="center">
+                    <CircularProgress />
+                </Grid>
+            )
+            }
+            {!loading && (
+                <Grid container direction="row" wrap="wrap">
+                    {departmentLoads.length ? departmentLoads.map(o =>
+                        <Grid item xs={3} className={classes.margin1}>
+                            <Card>
+                                <CardContent>
+                                    <DepartmentLoadDetails
+                                        departmentLoad={o}
+                                    />
+                                </CardContent>
+                                <CardActions>
+                                    <IconButton onClick={() => handleEdit(o)}>
+                                        <Edit />
+                                    </IconButton>
+                                    <IconButton onClick={() => handleDelete(o.id)}>
+                                        <Delete />
+                                    </IconButton>
+                                </CardActions>
+                            </Card>
                         </Grid>
-                    )}
-            </Grid>
-        </Grid>
+                    ) : (
+                            <Grid container direction="row" justify="center">
+                                <Typography color="textSecondary">Нагрузка на кафедру еще не была выделена.</Typography>
+                            </Grid>
+                        )}
+                </Grid>
+            )
+            }
+            <MessageSnackbar
+                variant={snackbar.variant}
+                message={snackbar.message}
+                open={snackbar.open}
+                onClose={() => setSnackbar('', false, undefined)}
+            />
+            <ImportLoadDetails
+                departmentId={departmentId}
+                onAccept={handleImportDetailsAccept}
+                onCancel={() => setImportDetailsOpen(false)}
+                open={importDetailsOpen}
+            />
+        </Grid >
     );
 }));

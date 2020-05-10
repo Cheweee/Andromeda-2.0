f153aaad6d71faf6e6a846167fd74e075cb5c848
Cheweee <kni2012.15@gmail.com>
2020-05-10 05:11:50 +0300

    Fixed error with not updating after create;

--- andromeda/src/store/trainingDepartmentStore/reducers.ts
+++ andromeda/src/store/trainingDepartmentStore/reducers.ts
@@ -52,13 +52,13 @@ export function trainingDepartmentReducer(prevState: TrainingDepartmentState = i
         }
 
         case ActionType.updateTrainingDepartmentUsers: {
-            if(prevState.trainingDepartmentLoading === true) {
+            if (prevState.trainingDepartmentLoading === true) {
                 return prevState;
             }
 
             const users = prevState.trainingDepartment.users.filter(o => o.id !== action.user.id);
 
-            for(const role of action.roles) {
+            for (const role of action.roles) {
                 users.push({
                     departmentId: role.departmentId,
                     departmentName: role.departmentName,
@@ -69,107 +69,114 @@ export function trainingDepartmentReducer(prevState: TrainingDepartmentState = i
                     userId: action.user.id
                 });
             }
-            
+
             const trainingDepartment = { ...prevState.trainingDepartment, users };
 
             const state: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment };
-            return { ...prevState, ...state }; 
+            return { ...prevState, ...state };
         }
         case ActionType.deleteTrainingDepartmentUsers: {
-            if(prevState.trainingDepartmentLoading === true) {
+            if (prevState.trainingDepartmentLoading === true) {
                 return prevState;
             }
 
             const users = prevState.trainingDepartment.users.filter(o => o.userId !== action.id);
-            
+
             const trainingDepartment = { ...prevState.trainingDepartment, users };
 
             const state: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment };
-            return { ...prevState, ...state }; 
+            return { ...prevState, ...state };
         }
 
         case ActionType.updateTrainingDepartmentDisciplinesTitles: {
-            if(prevState.trainingDepartmentLoading === true) return prevState;
+            if (prevState.trainingDepartmentLoading === true) return prevState;
 
             const titles = prevState.trainingDepartment.titles.filter(o => o.name !== action.disciplineTitle.name);
 
             titles.push(action.disciplineTitle);
 
             const department = { ...prevState.trainingDepartment, titles };
-            
+
             const state: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment: department };
             return { ...prevState, ...state };
         }
         case ActionType.deleteTrainingDepartmentDisciplinesTitles: {
-            if(prevState.trainingDepartmentLoading === true) return prevState;
+            if (prevState.trainingDepartmentLoading === true) return prevState;
 
             const titles = prevState.trainingDepartment.titles.filter(o => o.name !== action.name);
 
             const department = { ...prevState.trainingDepartment, titles };
-                    
+
             const state: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment: department };
             return { ...prevState, ...state };
         }
 
         case ActionType.updateTrainingDepartmentStudyDirections: {
-            if(prevState.trainingDepartmentLoading === true) return prevState;
+            if (prevState.trainingDepartmentLoading === true) return prevState;
 
             const studyDirections = prevState.trainingDepartment.studyDirections.filter(o => o.name !== action.studyDirection.name);
 
             studyDirections.push(action.studyDirection);
 
             const department = { ...prevState.trainingDepartment, studyDirections };
-            
+
             const state: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment: department };
             return { ...prevState, ...state };
         }
         case ActionType.deleteTrainingDepartmentStudyDirections: {
-            if(prevState.trainingDepartmentLoading === true) return prevState;
+            if (prevState.trainingDepartmentLoading === true) return prevState;
 
             const studyDirections = prevState.trainingDepartment.studyDirections.filter(o => o.name !== action.name);
-            
+
             const department = { ...prevState.trainingDepartment, studyDirections };
-            
+
             const state: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment: department };
             return { ...prevState, ...state };
         }
 
         case ActionType.updateTrainingDepartmentStudentGroups: {
-            if(prevState.trainingDepartmentLoading === true) return prevState;
+            if (prevState.trainingDepartmentLoading === true) return prevState;
 
             const groups = prevState.trainingDepartment.groups.filter(o => o.name !== action.studentGroup.name);
 
             groups.push(action.studentGroup);
 
             const department = { ...prevState.trainingDepartment, groups };
-            
+
             const state: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment: department };
             return { ...prevState, ...state };
         }
         case ActionType.deleteTrainingDepartmentStudentGroups: {
-            if(prevState.trainingDepartmentLoading === true) return prevState;
+            if (prevState.trainingDepartmentLoading === true) return prevState;
 
             const groups = prevState.trainingDepartment.groups.filter(o => o.name !== action.name);
 
             const department = { ...prevState.trainingDepartment, groups };
-            
+
             const state: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment: department };
             return { ...prevState, ...state };
         }
 
         case ActionType.saveTrainingDepartmentRequest: return prevState;
         case ActionType.createTrainingDepartmentSuccess: {
-            if (prevState.trainingDepartmentsLoading === true) return prevState;
+            if (prevState.trainingDepartmentsLoading === true || prevState.trainingDepartmentLoading === true) return prevState;
 
-            const state: TrainingDepartmentsState = { trainingDepartmentsLoading: false, trainingDepartments: prevState.trainingDepartments.concat(action.department) };
-            return { ...prevState, ...state };
+            const updatedModel = { ...prevState.trainingDepartment, ...action.department };
+            const updatedModels = prevState.trainingDepartments.concat(action.department);
+
+            const modelsState: TrainingDepartmentsState = { trainingDepartmentsLoading: false, trainingDepartments: updatedModels };
+            const modelState: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment: updatedModel };
+            return { ...prevState, ...modelsState, ...modelState };
         }
         case ActionType.updateTrainingDepartmentSuccess: {
-            if (prevState.trainingDepartmentsLoading === true) return prevState;
+            if (prevState.trainingDepartmentsLoading === true || prevState.trainingDepartmentLoading === true) return prevState;
 
-            const updated = prevState.trainingDepartments.map(o => o.id == action.department.id ? action.department : o);
-            const state: TrainingDepartmentsState = { trainingDepartmentsLoading: false, trainingDepartments: updated };
-            return { ...prevState, ...state };
+            const updatedModel = { ...prevState.trainingDepartment, ...action.department };
+            const updatedModels = prevState.trainingDepartments.map(o => o.id == action.department.id ? action.department : o);
+
+            const modelsState: TrainingDepartmentsState = { trainingDepartmentsLoading: false, trainingDepartments: updatedModels };
+            const modelState: SelectedTrainingDepartmentState = { trainingDepartmentLoading: false, trainingDepartment: updatedModel };
+            return { ...prevState, ...modelsState, ...modelState };
         }
         case ActionType.saveTrainingDepartmentFailure: return prevState;
 
